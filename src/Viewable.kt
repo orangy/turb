e4ad9b124567539a823aa37753e947cd18278f3b
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-05-14 00:13:15 +0400

    Filter & map on view

--- src/Viewable.kt
+++ src/Viewable.kt
@@ -3,3 +3,21 @@ package org.jetbrains.turb
 public trait Viewable<T> {
     public fun view(lifetime: Lifetime, observer: (Lifetime, T) -> Unit)
 }
+
+fun <T> Viewable<T>.filter(predicate: (T) -> Boolean): Viewable<T> = object : Viewable<T> {
+    override fun view(lifetime: Lifetime, observer: (Lifetime, T) -> Unit) {
+        this@filter.view(lifetime) {(l, v) ->
+            if (predicate(v)) {
+                observer(l, v)
+            }
+        }
+    }
+}
+
+fun <T, V> Viewable<T>.map(transform: (T) -> V): Viewable<V> = object : Viewable<V> {
+    override fun view(lifetime: Lifetime, observer: (Lifetime, V) -> Unit) {
+        this@map.view(lifetime) {(l, v) ->
+            observer(l, transform(v))
+        }
+    }
+}
